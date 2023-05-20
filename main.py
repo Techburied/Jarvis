1badaf44a485cf2d7fd63615606beb2b55e34d2a
tech buried <93422387+Techburied@users.noreply.github.com>
2023-05-20 11:15:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-import pyttsx3 # pip install pyttsx3
 
-def speak(Text):
-    engine = pyttsx3.init("sapi5") # module windows
-    voice = engine.getProperty("voices") # property chahiye voice
-    engine.setProperty("voice", voice[0].id) # set property "voice"
-    engine.setProperty("rate", 200)
-    print(f"jarvis: {Text}")
-    engine.say(text=Text)
-    engine.runAndWait()
-
-speak("hello world!")
-
-# speech recognition module / engine
